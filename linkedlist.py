72e3df7d7b459f455c3eced1080ba2d0803f4576
GuerroTaquero <49926403+GuerroTaquero@users.noreply.github.com>
2023-08-16 14:33:01 -0700

    second commit

--- linkedlist.py
+++ linkedlist.py
@@ -16,3 +16,18 @@ class Solution:
         
         return before.next
 
+
+from sortedcontainers import SortedList
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        l = SortedList()
+        l.update(nums[0:k])
+        print(nums[0:k])
+        ans = [l[-1]]
+        for i in range(k,len(nums)):
+            l.remove(nums[i-k])
+            l.add(nums[i])
+            #print(l)
+            ans.append(l[-1])
+
+        return ans
