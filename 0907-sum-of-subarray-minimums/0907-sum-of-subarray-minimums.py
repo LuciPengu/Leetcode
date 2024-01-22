8be38cf812e5623576f6080ac322afefefdf139d
Luci <49926403+LuciPengu@users.noreply.github.com>
2024-01-21 18:01:11 -0800

    Time: 333 ms (60.93%), Space: 21.5 MB (57.86%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -3,15 +3,15 @@ class Solution:
         
         stack = []
         res = 0
-        arr = [float("-inf")] + arr + [float("-inf")]
+        arr = arr + [float("-inf")]
         for i, n in enumerate(arr):
             while stack and n < stack[-1][1]:
                 j, m = stack.pop()
-                #print(j,m)
+                
                 left = j - stack[-1][0] if stack else j+1
                 right = i - j
                 
                 res = (res + m * left * right)
             stack.append((i,n))
-        print(res, stack)
+        
         return res %(10 ** 9 + 7)
