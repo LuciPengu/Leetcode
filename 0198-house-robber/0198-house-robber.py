0029d6fec3dbf1a34d7fb5bd01f87fd449a0ed0b
Luci <49926403+LuciPengu@users.noreply.github.com>
2024-01-21 19:49:05 -0800

    Time: 42 ms (36.69%), Space: 16.5 MB (57.82%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,9 +1,16 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        @cache
-        def dfs(i,c):
-            if i >= len(nums):
-                return c  
-            return max(dfs(i+1,c),dfs(i+2,c+nums[i]))
-        return dfs(0,0)
-            
+    def rob(self, a: List[int]) -> int:
+        def solve(ind,a):
+            if ind==0:
+                return a[ind]
+            if ind<0:
+                return 0
+            if dp[ind]!=-1:
+                return dp[ind]
+            pick=a[ind]+solve(ind-2,a)
+            non_pick=0+solve(ind-1,a)
+            dp[ind]=max(pick,non_pick)
+            return dp[ind]
+        n=len(a)
+        dp=[-1]*n
+        return solve(n-1,a)
