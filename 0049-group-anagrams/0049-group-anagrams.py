7e71308f726d51cbb1be048c257589cf2d3c7601
Luci <49926403+LuciPengu@users.noreply.github.com>
2024-02-07 22:57:18 -0800

    Time: 90 ms (61.77%), Space: 20.6 MB (48.94%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,7 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        
+        for i in strs:
+            res[tuple(sorted(i))].append(i)
+        return res.values()
