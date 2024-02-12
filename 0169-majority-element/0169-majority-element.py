4e60250bd506a234e09806bc57dbe534d14ec168
Luci <49926403+LuciPengu@users.noreply.github.com>
2024-02-12 14:08:09 -0800

    Time: 138 ms (52.21%), Space: 18 MB (84.09%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,3 +1,3 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return max(((set(nums))), key = lambda x:nums.count(x))
+        return max(set(nums), key = lambda x:nums.count(x))
