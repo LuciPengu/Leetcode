41e961b2f3fc9fec04d62a8c4bb12df69e0731ef
Luci <49926403+LuciPengu@users.noreply.github.com>
2024-02-25 17:17:18 -0800

    Time: 5253 ms (34.38%), Space: 69.2 MB (6.25%) - LeetHub

---
+++ 2709-greatest-common-divisor-traversal/2709-greatest-common-divisor-traversal.py
@@ -0,0 +1,40 @@
+class Solution:
+    def canTraverseAllPairs(self, nums: List[int]) -> bool:
+        dicto = {}
+        def prime_factors(n):
+            i = 2
+            factors = set()
+            while i * i <= n:
+                if n % i:
+                    i += 1
+                else:
+                    n //= i
+                    factors.add(i)
+            if n > 1:
+                factors.add(n)
+            return factors
+        
+        
+        seen = {}
+        adjlist = defaultdict(list)
+        for i in range(len(nums)):
+            for prime in prime_factors(nums[i]):
+                if prime in seen:
+                    adjlist[i].append(seen[prime])
+                    adjlist[seen[prime]].append(i)
+                else:
+                    seen[prime] = i
+        
+        print(adjlist)
+                
+        visited = set()
+        def dfs(i):
+            if i in visited:
+                return
+            visited.add(i)
+            for j in adjlist[i]:
+                dfs(j)
+                
+        dfs(0)
+        
+        return len(visited) == len(nums)
