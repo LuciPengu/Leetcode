cefb52a8ea72368b3ca45ca40ac025c5d433d7b0
Luci <49926403+LuciPengu@users.noreply.github.com>
2024-04-25 22:40:25 -0700

    Time: 6 ms (89.38%), Space: 13.7 MB (44.20%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> freq;
+        
         for(int i = 0; i < nums.size(); i++){
-            for(int j = 0; j < nums.size(); j++){
-                if (i == j) continue;
-                
-                if (nums[i] + nums[j] == target){
-                    return {i,j};
-                }
+            int val = target - nums[i];
+            if (freq.count(val)){
+                return {i, freq[val]};
             }
+            freq[nums[i]] = i;
         }
         return {-1,-1};
     }
