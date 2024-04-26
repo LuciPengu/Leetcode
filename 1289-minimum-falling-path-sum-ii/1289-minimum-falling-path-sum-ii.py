f1be768edcbec878795fbe6ff679f238f7b2b9cb
Luci <49926403+LuciPengu@users.noreply.github.com>
2024-04-25 22:25:41 -0700

    Time: 5931 ms (13.71%), Space: 40.2 MB (5.31%) - LeetHub

---
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minFallingPathSum(self, grid: List[List[int]]) -> int:
+        height = len(grid)
+        width = len(grid[0])
+        found = set()
+        @cache
+        def dfs(x,y):
+            if x == width-1:
+                return grid[x][y]
+
+            res = float("inf")
+            for nextX in range(width):
+                if nextX != y:
+                    res = min(res, dfs(x+1, nextX))
+
+            return res + grid[x][y]
+        
+        res = float("inf")
+        for col in range(width):
+            res = min(res, dfs(0,col))
+        print(found)
+        return res
