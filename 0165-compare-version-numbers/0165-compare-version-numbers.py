6d87c09b935728c0b34a205f9dfaf0ad00dcd45b
Luci <49926403+LuciPengu@users.noreply.github.com>
2024-05-03 17:40:30 -0700

    Time: 27 ms (94.01%), Space: 16.6 MB (13.54%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.py
+++ 0165-compare-version-numbers/0165-compare-version-numbers.py
@@ -9,30 +9,9 @@ class Solution:
         v1 = v1 + ["0"]*(n-len(v1))
         
 
-        v1arr, v2arr = [], []
-        for i in range(n):
-            
+        for i in range(n):            
             if int(v2[i]) < int(v1[i]):
                 return 1
             elif int(v2[i]) > int(v1[i]):
-                return -1
-        return 0
-        
-        if v1arr == v2arr:
-            return 0
-        
-        for i in range(len(v1arr)):
-            try:
-                if v1arr[i] > v2arr[i]:
-                    return 1
-                elif v1arr[i] < v2arr[i]:
-                    return -1
-            except:
-                if v1arr[i] > 0:
-                    return 1
-        
-        print(i)
-        for j in range(i+1, len(v2arr)):
-            if v2arr[j] > 0:
                 return -1
         return 0
